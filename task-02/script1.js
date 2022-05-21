932aec8cda58b2280fff54c64309b1b6c3da4aa0
zhiide <88451170+zhiide@users.noreply.github.com>
2022-05-22 00:11:13 +0600

    02

---
+++ task-02/script1.js
@@ -0,0 +1,4 @@
+// 1. Дана строка **'ddd@bbb@ccc'**. Замените все **@** на **'!'**
+
+let str1 = '1) aaa@bbb@ccc';
+alert(str1.replace(/@/g, '!'));
---
+++ task-02/script4.js
@@ -0,0 +1,5 @@
+// 4. Дана строка**'I am a hero!'**. Выведите в консоль **количество символов** в этой строке.
+
+let str4 = 'I am a hero!';
+alert(`4) Sentence "I am a hero!" has ${str4.length} characters`);
+
